7e40d83fb11a13319d5b4adb211d82d58b98fa9b
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-05-15 12:36:16 -0700

    lets check

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Web server 
+Web server
 """
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
@@ -8,10 +8,6 @@ from flask import Flask, jsonify, make_response
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-@app.route('/status', methods=['GET'])
-def api_status():
-    """ Endpoint to check API status"""
-    return (jsonify({"status": "OK"})
 
 @app.errorhandler(404)
 def not_found(error):
@@ -20,5 +16,5 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    # python -m api.v1.app 
+    # python -m api.v1.app
     app.run(host="0.0.0.0", port=5000)
