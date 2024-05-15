ee1598343cf0a318c235ec859c90a372e3627529
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-05-15 12:26:36 -0700

    Check reqs

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,10 +3,12 @@
 
 
 class square():
+    """ Square Class attrs """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Sets attrs based on passed args """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,14 +17,15 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """ Calc perimeter of square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Returns string representation of square obj """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
     s = square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
